38c95cca750ec00154f474d6f1ef5dd63c2af6cc
Andrew Sutyrin <andoitt@gmail.com>
2024-05-03 16:11:29 +0300

    ST-03 Ui obj fixing-3 + start custom views - 2

--- app/src/main/java/com/example/incrementapp/views/CustomTextView.kt
+++ app/src/main/java/com/example/incrementapp/views/CustomTextView.kt
@@ -1,6 +1,28 @@
 package com.example.incrementapp.views
 
-import android.widget.LinearLayout
+import android.content.Context
+import android.os.Parcelable
+import android.util.AttributeSet
+import com.google.android.material.button.MaterialButton
 
-class CustomTextView : LinearLayout {
+class CustomTextView : androidx.appcompat.widget.AppCompatTextView {
+
+     constructor(context: Context) : super(context)
+         constructor(context: Context, attributeSet: AttributeSet) : super(context, attributeSet)
+         constructor(context: Context, attributeSet: AttributeSet, defStyleAttrs: Int) : super(
+             context,
+             attributeSet,
+             defStyleAttrs
+         )
+
+
+
+
+}
+
+
+interface TextAction {
+    fun updateUiState : TextUiState(){
+
+    }
 }
---
+++ app/src/main/java/com/example/incrementapp/views/TextUiState.kt
@@ -0,0 +1,7 @@
+package com.example.incrementapp.views
+
+interface TextUiState {
+
+    fun
+
+}
